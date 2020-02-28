ca3213f16f07f824d91f31b77d9e6f12d33deaa6
MiaoMiao <1014418876@qq.com>
2020-02-28 17:17:54 +0800

    test

--- src/sandboxProcess.ts
+++ src/sandboxProcess.ts
@@ -77,7 +77,7 @@ export class SandboxProcess {
                     const memUsage = memUsageWithCache - cache;
                     
 
-                    const testMemory: number = Number(sandboxAddon.GetCgroupProperty("memory", myFather.parameter.cgroup, "memory.memsw.usage_in_bytes"));
+                    const testMemory: number = Number(sandboxAddon.GetCgroupProperty("memory", myFather.parameter.cgroup, "memory.stat"));
 
                     console.info("**********************************");
                     console.info("testMemory=>", testMemory);
